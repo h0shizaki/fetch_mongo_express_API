1016dc485b0634cda292560c05601dedb20634f9
Kitsune-fx <kant.cmd@gmail.com>
2021-11-09 12:07:33 +0700

    [🍬 REST API test]

--- index.js
+++ index.js
@@ -4,9 +4,37 @@ function getData(){
     fetch(API_URL+'')
     .then(res => res.json())
     .then(data =>{
-        console.log(data[0])
+        const members = data;
+        let trData=''
+
+        members.forEach( member=>{
+            trData += '<tr>';
+            trData += `<td>${member['id']}</td>`
+            trData += `<td>${member['first_name']}</td>`
+            trData += `<td>${member['last_name']}</td>`
+            trData += `<td>${member['level']}</td>`
+            trData += `<td><a class ='btn btn-warning' href='editMem.html?id=${member['id']}'>Edit</a></td>`
+            trData += `<td><button class='btn btn-danger' onclick='deleteData(${member['level']})'>Delete</button></td>`
+        })
+
+        document.querySelector('#myTable').innerHTML = trData;
 
     });
 
 }
 getData()
+
+async function deleteData(id){
+    let confirm = confirm('Want to delete? Are you sure?')
+
+    if(confirm){
+        await fetch(API_URL+'delete' , {
+            method: 'DELETE',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify({'id': id})
+        } )
+    
+    getData()
+
+    }
+}
